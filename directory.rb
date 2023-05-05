471df4d75549081ed2bcb3fe11ad8d2838a14859
delladi <delaram.ae@gmail.com>
2023-05-05 10:28:55 +0000

    Adding a method for preloading file with a filename as an argument

--- directory.rb
+++ directory.rb
@@ -4,21 +4,21 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -47,6 +47,27 @@ def save_students
   end
   file.close
 end
+#method for loading files
+def load_students(filename = "students.csv")
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+#method for loading the program with argument
+def try_load_students
+  filename = ARGV.first #first argument from the command line
+  return if filename.nil? #get out of the method if it isn't given
+  if File.exist?(filename) #if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else #if it doesnt exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit #quit the program
+  end
+end
 #method for selection
 def process(selection)
   case selection
@@ -56,6 +77,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -77,5 +100,6 @@ end
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
-
+#nothing gonna happen until we call our method for the menu
+try_load_students
 interactive_menu
